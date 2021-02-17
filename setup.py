bd5a435bc10793526aebbf32cc3014cd72d56986
Tarith <tarithj@gmail.com>
2021-02-17 13:30:21 +0530

    changed version from v1.1 to v1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Dem',
-      version='1.1',
+      version='1.2',
       url='https://github.com/flew-software/D',
       license='MIT',
       author='Tarith Jayasooriya',
